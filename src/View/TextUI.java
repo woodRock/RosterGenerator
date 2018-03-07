8f1195819b19006b0a0e6fbdc20e8911c5d30110
Jesse Wood <jessewood0876@gmail.com>
2018-03-07 15:09:16 +1300

    First commit

--- src/View/TextUI.java
+++ src/View/TextUI.java
@@ -1,4 +1,32 @@
 package View;
 
+import Model.Roster;
+
+/**
+ * This class captures allows for the roster to be represented as text
+ */
 public class TextUI {
+
+    private String rosterText;
+
+    /**
+     * The default constructor for the text representation
+     * of the user interface
+     * @param roster to be converted to text
+     */
+    public TextUI(Roster roster){
+        this.rosterText = buildRoster(roster);
+    }
+
+    /**
+     * This method builds the string for the TextUI
+     * @param roster to be represented by text
+     * @return a text representation of the roster
+     */
+    private String buildRoster(Roster roster){
+        return "";
+    }
+
+
+
 }
